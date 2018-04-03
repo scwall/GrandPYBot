5e547aa2489ffdad5b614357ba5dd72c80f9ff38
scwall <deselys.pascal@gmail.com>
2018-04-03 14:41:17 +0200

    Change template, add new test, add class 'parser'

--- TellMe/tests/test_tellme.py
+++ TellMe/tests/test_tellme.py
@@ -1,8 +1,8 @@
-# test_app.py
+
 import pytest
 import sys
 from flask import url_for, json
-from ..views import result_google_maps
+from parser import Parser
 import urllib3
 import logging
 logging.basicConfig(format='%(message)s')
@@ -11,24 +11,26 @@ http = urllib3.PoolManager()
 
 class TestApp:
 
-    def test_connection(self, client):
+    def test_page_index(self, client):
         self.r = client.get()
         assert self.r.status == '200 OK'
 
-    def test_jquery(self,client):
+    def test_parser(self):
+
+        assert Parser.parser("c'est un test") == "un test"
+
+    def test_question(self,client):
         mimetype = 'application/json'
         headers = {
             'Content-Type': mimetype,
             'Accept': mimetype
         }
         data = {
-            'grandfather_question': 'question'
+            'grandfather_question': "raconte moi l'histoire de la rue de liege 20 verviers"
         }
-        url = '/add'
+        url = '/question'
 
         response = client.post(url, data=json.dumps(data), headers=headers)
         assert response.content_type == mimetype
-        assert response.json == {'grandfather_question': 'question'}
-    def test_result_google_maps(self):
-        result_google_maps('')
+        assert response.json == "raconte l'histoire la rue liege 20 verviers"
 
--- TellMe/views.py
+++ TellMe/views.py
@@ -1,78 +1,28 @@
+import sys
 from flask import Flask, jsonify, request, render_template, Response, json
 from flask_googlemaps import GoogleMaps, Map
-
-
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                'lat': 37.4419,
-                'lng': -122.1419,
-                'infobox': "<b>Hello World</b>"
-            },
-            {
-                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                'lat': 37.4300,
-                'lng': -122.1400,
-                'infobox': "<b>Hello World from other place</b>"
-            }
-        ]
-    )
-    return  sndmap
-
+from flask_classful import FlaskView, route
+from parser import Parser
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
-    @app.route('/')
-    def mapview():
-        # creating a map in the view
-        mymap = Map(
-            identifier="view-side",
-            lat=37.4419,
-            lng=-122.1419,
-            markers=[(37.4419, -122.1419)]
-        )
-        sndmap = Map(
-            identifier="sndmap",
-            lat=37.4419,
-            lng=-122.1419,
-            markers=[
-                {
-                    'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                    'lat': 37.4419,
-                    'lng': -122.1419,
-                    'infobox': "<b>Hello World</b>"
-                },
-                {
-                    'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                    'lat': 37.4300,
-                    'lng': -122.1400,
-                    'infobox': "<b>Hello World from other place</b>"
-                }
-            ]
-        )
-        return render_template('example.html', mymap=mymap, sndmap=mapview())
-    # def GrandPyBot():
-    #     return render_template('test.html', sndmap=mapview())
 
-    @app.route('/add', methods=['POST'])
-    def add():
-        json = request.get_json()
+    class MainView(FlaskView):
+        @route('/')
+        def index(self):
+            return render_template('index.html')
 
-        return jsonify(json)
+        @route('question', methods=["POST"])
+        def question(self):
+            self.question = request.get_json().get('grandfather_question')
+            self.question = Parser.parser(self.question)
+            return jsonify(self.question)
+
+    MainView.register(app, route_base='/')
     return app
+
+
 if __name__ == '__main__':
     app = create_app('config')
     GoogleMaps(app)
