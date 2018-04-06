671f2a759d097f19b26795777ee232b23fe20cf2
scwall <deselys.pascal@gmail.com>
2018-04-07 00:15:41 +0200

    modify, impoty json and remove line

--- bin/Parser/parser.py
+++ bin/Parser/parser.py
@@ -1,21 +1,19 @@
 import os
+import re
 import unidecode
-from pathlib import Path
 from flask import json
 
-import re
 
 class Parser:
     def __init__(self, language):
         file_dir_path = os.path.dirname(os.path.abspath(__file__))
         self.language = os.path.join(file_dir_path + "/{}.json".format(language))
         words_and_caracters = json.load(open(self.language))
-        self.LIST_WORD =  words_and_caracters["LIST_WORD"]
-        self.LIST_CARACTER =  words_and_caracters["LIST_CARACTER"]
+        self.LIST_WORD = words_and_caracters["LIST_WORD"]
 
     def parser_word(self, text):
-        text_list =  unidecode.unidecode(text.lower())
-        text_list = re.split(r"[^a-zA-Z0-9]",text_list)
+        text_list = unidecode.unidecode(text.lower())
+        text_list = re.split(r"[^a-zA-Z0-9]", text_list)
         number_list_word = 0
         word_is_remove = False
 
@@ -34,7 +32,7 @@ class Parser:
         return " ".join(text_list)
 
     @staticmethod
-    def parser_refine(text,method=None):
+    def parser_refine(text, method=None):
         if method is 'reverse':
             text_list = text.lower().split()
             text_list.pop(-1)
@@ -43,10 +41,9 @@ class Parser:
             text_list.pop(0)
 
         return " ".join(text_list)
+
     @staticmethod
     def parser_number(text):
         text_list = unidecode.unidecode(text.lower())
         text_list = re.split('[^a-zA-Z]', text_list)
         return " ".join(text_list)
-
-
