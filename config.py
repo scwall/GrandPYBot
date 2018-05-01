8e79dce6c8b0417c1dfe93094b8453370206f588
scwall <deselys.pascal@gmail.com>
2018-05-01 13:54:20 +0200

    add procfile, add requirements for heroku

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_databases.db')
+# database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_databases.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
